bb211006d7768ade813738792cac292e9ac6e39d
Nino Mercado <njesusmercado@gmail.com>
2024-08-24 15:48:22 -0500

    refact(index.js): formatted

--- getListOfWordsByWord_Lambda/index.js
+++ getListOfWordsByWord_Lambda/index.js
@@ -2,13 +2,13 @@ const connectToDatabase = require("./db.js").connectToDatabase;
 
 async function findListOfWordsByLetter({ db, items = "", limit = 1, page = 0 }) {
 
-    const fieldsToFilterSearch = { 'palabra': { $regex: items } }
-    const fieldsToSelect = { 'projection': { 'palabra': 1, 'popularidad': 1, 'definicion': 1, 'ejemplos': 1 }}
+  const fieldsToFilterSearch = { 'palabra': { $regex: items } }
+  const fieldsToSelect = { 'projection': { 'palabra': 1, 'popularidad': 1, 'definicion': 1, 'ejemplos': 1 } }
 
-    return await db.collection('Palabra').find(fieldsToFilterSearch, fieldsToSelect)
-      .skip(page)
-      .limit(limit)
-      .toArray();
+  return await db.collection('Palabra').find(fieldsToFilterSearch, fieldsToSelect)
+    .skip(page)
+    .limit(limit)
+    .toArray();
 };
 
 exports.handler = async (event, context) => {
@@ -17,7 +17,7 @@ exports.handler = async (event, context) => {
 
   const db = await connectToDatabase();
 
-  const response = await findListOfWordsByLetter({db, items: "A", limit: 10, page: 0 })
+  const response = await findListOfWordsByLetter({ db, items: "A", limit: 10, page: 0 })
 
   return {
     statusCode: 200,
