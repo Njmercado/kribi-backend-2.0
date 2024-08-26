b4d8901162f6c38e1e3354b5b53e957219ce16a7
Nino Mercado <njesusmercado@gmail.com>
2024-08-26 13:06:39 -0500

    refact(getListOfWordsByLetter_Lambda): catching params and changed name of lambda file

--- getListOfWordsByLetter_Lambda/index.js
+++ getListOfWordsByLetter_Lambda/index.js
@@ -1,6 +1,6 @@
 const connectToDatabase = require("./db.js").connectToDatabase;
 
-async function findListOfWordsByLetter({ db, items = "", limit = 1, page = 0 }) {
+async function findListOfWordsByLetter({ db, letter = "", limit = 1, page = 0 }) {
 
   const fieldsToFilterSearch = { 'palabra': { $regex: items } }
   const fieldsToSelect = { 'projection': { 'palabra': 1, 'popularidad': 1, 'definicion': 1, 'ejemplos': 1 } }
@@ -13,11 +13,13 @@ async function findListOfWordsByLetter({ db, items = "", limit = 1, page = 0 }) 
 
 exports.handler = async (event, context) => {
 
+  const { letter, page } = event.queryStringParameters;
+
   context.callbackWaitsForEmptyEventLoop = false;
 
   const db = await connectToDatabase();
 
-  const response = await findListOfWordsByLetter({ db, items: "A", limit: 10, page: 0 })
+  const response = await findListOfWordsByLetter({ db, letter, limit: 10, page: parseInt(page) })
 
   return {
     statusCode: 200,
