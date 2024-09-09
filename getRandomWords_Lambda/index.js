a59ce7a7a99f2c4c25666cf5279e38cc6f083e04
Nino Mercado <njesusmercado@gmail.com>
2024-09-08 22:19:33 -0500

    fix(getRandomWords_Lambda): fixed pipeline

--- getRandomWords_Lambda/index.js
+++ getRandomWords_Lambda/index.js
@@ -24,7 +24,7 @@ exports.handler = async (event, context) => {
       }
     }
   } catch (error) {
-    console.error("ERROR: ", error);
+    console.error(error);
     return {
       isBase64Encoded: false,
       status: 500,
